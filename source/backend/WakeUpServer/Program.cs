c89d18a20b366c882a0383395e4622364d7e76f9
Martin Bühlmann <martin.buehlmann@erowa.com>
2024-04-18 08:54:04 +0200

    NuGet packages updated.

--- source/backend/WakeUpServer/Program.cs
+++ source/backend/WakeUpServer/Program.cs
@@ -10,7 +10,7 @@ using Serilog;
 using Serilog.Events;
 using WakeUpServer.BackgroundServices;
 
-public class Program
+public static class Program
 {
     public static void Main(string[] args)
     {
