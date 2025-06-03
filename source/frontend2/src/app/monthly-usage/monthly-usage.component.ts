3f4a4759e1c6d3595e5e12df3ca0d9e5fd6d3649
Martin Bühlmann <martin.buehlmann@erowa.com>
2025-06-03 13:05:52 +0200

    add new ui

---
+++ source/frontend2/src/app/monthly-usage/monthly-usage.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-monthly-usage',
+  imports: [],
+  templateUrl: './monthly-usage.component.html',
+  styleUrl: './monthly-usage.component.scss'
+})
+export class MonthlyUsageComponent {
+
+}
