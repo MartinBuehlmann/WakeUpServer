7ac941f84aaf8626a3ae6b20f4d3e7fe50e39fac
Martin Bühlmann <martin.buehlmann@erowa.com>
2025-06-04 06:41:34 +0200

    Reintroduce features folder.

--- source/frontend/src/app/home/home.component.ts
+++ source/frontend/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { MonthlyUsageComponent } from "../monthly-usage/monthly-usage.component";
+import { MonthlyUsageComponent } from "../features/reporting/monthly-usage/monthly-usage.component";
 
 @Component({
   selector: 'app-home',
