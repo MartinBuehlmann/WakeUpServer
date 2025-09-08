ef9ee4cb58fa2ec26264d7be82f6c159bbb5c567
Martin Bühlmann <martin.buehlmann@gmx.ch>
2025-09-08 08:53:50 +0200

    V18: Update to Angular v20 (#19)
    * Enable hash routing for proper refresh (F5)
    
    * NuGet packages updated.
    
    * Nuget packages updated.
    
    * Switch to central package management.
    
    * Update to Angular v20.
    
    * Nuget package updated.
    
    ---------
    
    Co-authored-by: Martin Bühlmann <martin.buehlmann@erowa.com>

--- source/frontend/src/app/app.config.ts
+++ source/frontend/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
@@ -31,7 +31,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
     provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
